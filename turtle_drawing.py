82649610505d2060efaa4c2f2020b56f833041b9
yt3kin <dugmerantekin@gmail.com>
2023-09-03 14:16:25 +0300

    catch_turtle

---
+++ turtle_drawing.py
@@ -0,0 +1,59 @@
+import turtle
+#ekran ayarları
+drawing_board = turtle.Screen()
+drawing_board.bgcolor("green")
+drawing_board.title("Python Turtle")
+
+'''
+turtle_instance_2 = turtle.Turtle()
+turtle_instance_2.left(90)
+turtle_instance_2.forward(100)
+turtle_instance_2.left(90)
+turtle_instance_2.forward(100)
+turtle_instance_2.left(90)
+turtle_instance_2.forward(100)
+turtle_instance_2.left(90)
+turtle_instance_2.forward(100)
+turtle.done()
+
+kare
+tt = turtle.Turtle()
+for i in range(4):
+    tt.forward(100)
+    tt.left(90)
+turtle.done()
+yıldı
+tt = turtle.Turtle()
+for i in range(5):
+    tt.forward(100)
+    tt.right(144)
+
+turtle.done()
+
+'''
+
+#turtle_instance.forward(100)
+
+#square
+#for i in range(4):
+#    turtle_instance.right(90)
+#    turtle_instance.forward(50)
+
+#star
+#for i in range(5):
+#    turtle_instance.right(144)
+#    turtle_instance.forward(360)
+
+'''
+#polygon
+num_sides = 8
+side_length = 70
+angle = 360.0 / num_sides
+turtle_instance = turtle.Turtle()
+for i in range(num_sides):
+    turtle_instance.forward(side_length)
+    turtle_instance.right(angle)
+
+turtle.done()
+
+'''
